f800f51556414eb3d71097c730bf72e00eb84323
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-06-13 12:49:38 +0700

    Setting Up Project, Adding Themes

---
+++ lib/themes.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color purpleColor = const Color(0xFF5843BE);
+Color orangeColor = const Color(0xFFFF5B5B);
+Color blackColor = const Color(0xFF000000);
+Color whiteColor = const Color(0xFFFFFFFF);
+Color greyColor = const Color(0xFF82868E);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: blackColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: greyColor,
+  fontWeight: FontWeight.w300,
+);
+
+TextStyle purpleTextStyle = GoogleFonts.poppins(
+  color: purpleColor,
+  fontWeight: FontWeight.w500,
+);
